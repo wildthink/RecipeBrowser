0d00e197998362c0db55aa4b662004b0dcdd4c67
Jason Jobe <box2019@jasonjobe.com>
2024-12-19 14:25:53 -0500

    Initial Commit

---
+++ RecipeBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipeBrowser
+//
+//  Created by Jason Jobe on 12/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipeBrowser/RecipeBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBrowserApp.swift
+//  RecipeBrowser
+//
+//  Created by Jason Jobe on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
